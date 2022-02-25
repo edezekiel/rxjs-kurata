f9e33eef4db6fa135f832bde733bf83df9b8e032
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2022-02-25 15:02:33 -0600

    Add APM start src

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { of, from } from 'rxjs';
-import { map, tap, take } from 'rxjs/operators';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'pm-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = 'RxJS in Angular (Kurata)';
-
-  ngOnInit() {
-    of(2, 4, 5, 6)
-      .pipe(
-        map((x) => x * 2),
-        tap((x) => console.log('tap, x = ', x)),
-        take(3)
-      )
-      .subscribe((x) => console.log('observed, x = ', x));
-  }
+export class AppComponent {
+  pageTitle = 'Acme Product Management';
 }
