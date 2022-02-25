e59db8ec190ebd6b2ca83e94b064d4c26fe325b8
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2022-02-25 12:03:11 -0600

    Use of and from operators to create Observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { of, from } from 'rxjs';
+import { map, tap, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'rxjs-kurata';
+export class AppComponent implements OnInit {
+  title = 'RxJS in Angular (Kurata)';
+
+  ngOnInit() {
+    of(2, 4, 5, 6)
+      .pipe(
+        map((x) => x * 2),
+        tap((x) => console.log('tap, x = ', x)),
+        take(3)
+      )
+      .subscribe((x) => console.log('observed, x = ', x));
+  }
 }
