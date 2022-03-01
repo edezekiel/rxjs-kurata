64c350ad0c63cf7b4e05bff8c4eab0756a2cba69
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2022-03-01 15:58:43 -0600

    Use switchMap and forkJoin to get suppliers for a product 'Just in time'; use vm$ (view model) to reduce number of async pipes in the template

--- src/app/products/product-list-alt/product-detail.component.ts
+++ src/app/products/product-list-alt/product-detail.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { catchError, EMPTY, Subject } from 'rxjs';
+import { catchError, combineLatest, EMPTY, filter, map, Subject } from 'rxjs';
 import { Supplier } from 'src/app/suppliers/supplier';
 
 import { ProductService } from '../product.service';
@@ -7,20 +7,41 @@ import { ProductService } from '../product.service';
 @Component({
   selector: 'pm-product-detail',
   templateUrl: './product-detail.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductDetailComponent {
-  pageTitle = 'Product Detail';
   errorMessageSubject = new Subject<string>();
   errorMessage$ = this.errorMessageSubject.asObservable();
-
-  productSuppliers: Supplier[] | null = null;
 
   product$ = this.productService.selectedProduct$.pipe(
     catchError((err) => {
       this.errorMessageSubject.next(err);
       return EMPTY;
     })
+  );
+
+  pageTitle$ = this.product$.pipe(
+    map((p) => (p ? `Product Detail for: ${p.productName}` : null))
+  );
+
+  productSuppliers$ = this.productService.selectedProductSuppliers$.pipe(
+    catchError((err) => {
+      this.errorMessageSubject.next(err);
+      return EMPTY;
+    })
+  );
+
+  vm$ = combineLatest([
+    this.product$,
+    this.productSuppliers$,
+    this.pageTitle$,
+  ]).pipe(
+    filter(([product]) => Boolean(product)),
+    map(([product, productSuppliers, pageTitle]) => ({
+      product,
+      productSuppliers,
+      pageTitle,
+    }))
   );
 
   constructor(private productService: ProductService) {}
